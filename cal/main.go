7a89a9f6f6c5eaa54e2586ccbb45fd454d6ddfe2
Erik Winter <erik@deonticdata.com>
2024-09-25 07:27:39 +0200

    cal config

--- cal/main.go
+++ cal/main.go
@@ -3,17 +3,30 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 	"time"
 
 	"go-mod.ewintr.nl/planner/item"
+	"gopkg.in/yaml.v3"
 )
 
 func main() {
 	fmt.Println("cal")
-
-	repo, err := NewSqlite("test.db")
+	confPath, err := os.UserConfigDir()
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("could not get config path: %s\n", err)
+		os.Exit(1)
+	}
+	conf, err := LoadConfig(filepath.Join(confPath, "planner", "cal", "config.yaml"))
+	if err != nil {
+		fmt.Printf("could not open config file: %s\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("conf: %+v\n", conf)
+
+	repo, err := NewSqlite(conf.DBPath)
+	if err != nil {
+		fmt.Printf("could not open db file: %s\n", err)
 		os.Exit(1)
 	}
 
@@ -25,7 +38,7 @@ func main() {
 			End:   time.Now().Add(-5 * time.Second),
 		},
 	}
-	if err := repo.Delete(one.ID); err != nil {
+	if err := repo.Store(one); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
@@ -65,4 +78,21 @@ func main() {
 	// }
 
 	// fmt.Printf("%+v\n", items)
+}
+
+type Configuration struct {
+	DBPath string `yaml:"dbpath"`
+}
+
+func LoadConfig(path string) (Configuration, error) {
+	confFile, err := os.ReadFile(path)
+	if err != nil {
+		return Configuration{}, fmt.Errorf("could not open file: %s", err)
+	}
+	var conf Configuration
+	if err := yaml.Unmarshal(confFile, &conf); err != nil {
+		return Configuration{}, fmt.Errorf("could not unmarshal config: %s", err)
+	}
+
+	return conf, nil
 }
